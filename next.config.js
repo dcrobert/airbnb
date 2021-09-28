99b4c9ccf088d87f447e3414c34beef4a5c3c517
Dalton Robertson <dcrobertson@Daltons-MacBook-Pro-2.local>
2021-09-28 14:55:02 -0500

    day four airbnb build

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
         domains: ['links.papareact.com']
     },
+    env: {
+        mapbox_key: 'pk.eyJ1IjoiZGNyb2JlcnQyMiIsImEiOiJja3U0NWI1NXM0bm41MnhwbXA5dGdzNGN1In0.GE3rBaY4hEFn1SBk8KIcdA'
+    },
 };
